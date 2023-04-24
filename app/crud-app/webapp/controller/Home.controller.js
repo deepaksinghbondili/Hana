9ddb1ef7a7de1d3098b583d1d97347463b8ab6a3
Deepak <deepaksinghbondili@gmail.com>
2023-04-24 20:37:25 +0530

    test

--- app/crud-app/webapp/controller/Home.controller.js
+++ app/crud-app/webapp/controller/Home.controller.js
@@ -40,13 +40,20 @@ sap.ui.define([
                 var page = this.byId("dynamicPageId");
                 page.setShowFooter(!page.getShowFooter());
             },
-            onCreate: function () {
+            onCreate: function (oEvent) {
                 that.oCreateDialog.open();
             },
             onClose: function () {
                 that.oCreateDialog.close();
+                that.clear();
             },
-            onSave: function () {
+            clear : function(){
+                sap.ui.getCore().byId("idEmp").setValue("");
+                sap.ui.getCore().byId("idName").setValue("");
+                sap.ui.getCore().byId("idNum").setValue("");
+                sap.ui.getCore().byId("idCity").setValue("");
+            },
+            onSave: function (oEvent) {
                 var id = sap.ui.getCore().byId("idEmp").getValue();
                 var name = sap.ui.getCore().byId("idName").getValue();
                 var phone = sap.ui.getCore().byId("idNum").getValue();
@@ -57,6 +64,7 @@ sap.ui.define([
                     Phone: phone,
                     City: city
                 };
+
                 var oModel = that.getOwnerComponent().getModel("oModel");
                 oModel.callFunction("/validate", {
                     method : "GET",
@@ -66,6 +74,7 @@ sap.ui.define([
                     success: function (oData, _response) {
                         var message = _response.data.validate.message;
                         sap.m.MessageToast.show(message);
+                        that.clear();
                         that.oCreateDialog.close();
                         that.onAfterRendering();
                     },
@@ -81,6 +90,11 @@ sap.ui.define([
 
             onEdit: function (oEvent) {
                 that.oCreateDialog.open();
+                var oTable = that.getView().byId("idTable");
+                sap.ui.getCore().byId("idEmp").setValue(oEvent.getSource().getParent().getCells()[0].getText());
+                sap.ui.getCore().byId("idName").setValue(oEvent.getSource().getParent().getCells()[1].getText());
+                sap.ui.getCore().byId("idNum").setValue(oEvent.getSource().getParent().getCells()[2].getText());
+                sap.ui.getCore().byId("idCity").setValue(oEvent.getSource().getParent().getCells()[3].getText());
             },
 
             onSearch: function (oEvent) {
