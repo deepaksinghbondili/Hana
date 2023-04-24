dfbb3886619cafcb5c431d3a0a75447bd64db734
Deepak <deepaksinghbondili@gmail.com>
2023-04-24 13:58:16 +0530

    Test

--- srv/interactions_srv.js
+++ srv/interactions_srv.js
@@ -1,20 +1,23 @@
 const cds = require("@sap/cds");
 module.exports = srv => {
-    srv.on("validate", async (req,res) => {
+    srv.before("validate", async (req) => {
+        debugger;
+        console.log("This is before validate function execution");
+    });
+    srv.on("validate", async (req, res) => {
         try {
             debugger;
             const obj = JSON.parse(req.data.Obj);
             await cds.run(INSERT.into("APP_INTERACTIONS_ORGANIZATION").entries(obj));
-            const result = {message : "Successfully Created "}
+            const result = { message: "Successfully Created " }
             return result;
         } catch (error) {
             console.error(error);
             return error;
         }
     });
-    // srv.after("validate", res => {
-    //     if (res.status === 500) {
-    //         console.error(res.error);
-    //     }
-    // });
+    srv.after("validate", res => {
+        debugger;
+        console.error("This is after validate function is completed");
+    });
 };
